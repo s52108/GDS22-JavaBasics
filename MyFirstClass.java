dd42fd9761f838cdc7972742b515a9ce9f646264
postl <s52108@edu.campus02.at>
2022-09-23 17:10:13 +0200

    MyFirstClass created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    // Wir haben unsere erste Klasse erzeugt.
+    // psvm, Tür ins Haus
+    public static void main(String[] args) {
+        //erste Ausgabe
+        System.out.println("Ich werde auf der Konsole angezeigt.");
+
+        // args
+        // Kürzel sout+TAB
+        System.out.println(Arrays.toString(args));
+    }
+}
