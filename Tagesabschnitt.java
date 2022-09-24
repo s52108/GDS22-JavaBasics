9a9224dccb01467d79fafc9313880e46a8c617d3
postl <s52108@edu.campus02.at>
2022-09-24 17:34:36 +0200

    Improved Tagesabschnitt

--- Tagesabschnitt.java
+++ Tagesabschnitt.java
@@ -1,5 +1,27 @@
 public class Tagesabschnitt {
     public static void main(String[] args) {
+int uhrzeit = 0;
+        System.out.println("Uhrzeit = " + uhrzeit);
+        System.out.println("Tagesabschnitt = " + tagesabschnitt(uhrzeit));
+    }
 
+    public static String tagesabschnitt(int a){
+        if (a <= 9 && a >=6)
+        {
+            return ("Früh");
+        }
+        else if (a > 9 && a < 12){
+            return ("Vormittag");
+        }
+        else if (a >= 12 && a < 16){
+            return ("Nachmittag");
+        }
+        else if (a >= 16 && a < 20) {
+            return ("Abend");
+        }
+        else if (a>= 20 && a < 24 || a >=0 && a < 6){
+            return ("Nacht");
+        }
+        return ("Keine Gültige Uhrzeit");
     }
 }
