950833413ae620318ad5b33c11e9ccf95f0161e8
postl <s52108@edu.campus02.at>
2022-09-24 17:45:27 +0200

    Solved Tagesabschnitt

--- Tagesabschnitt.java
+++ Tagesabschnitt.java
@@ -1,25 +1,22 @@
 public class Tagesabschnitt {
     public static void main(String[] args) {
-int uhrzeit = 0;
+        int uhrzeit = 23;
         System.out.println("Uhrzeit = " + uhrzeit);
         System.out.println("Tagesabschnitt = " + tagesabschnitt(uhrzeit));
     }
 
-    public static String tagesabschnitt(int a){
-        if (a <= 9 && a >=6)
-        {
-            return ("Früh");
-        }
-        else if (a > 9 && a < 12){
-            return ("Vormittag");
-        }
-        else if (a >= 12 && a < 16){
-            return ("Nachmittag");
-        }
-        else if (a >= 16 && a < 20) {
-            return ("Abend");
-        }
-        else if (a>= 20 && a < 24 || a >=0 && a < 6){
+    public static String tagesabschnitt(int a) {
+        if (a >= 0 && a <= 24) {
+
+            if (a <= 9 && a >= 6) {
+                return ("Früh");
+            } else if (a > 9 && a < 12) {
+                return ("Vormittag");
+            } else if (a >= 12 && a < 16) {
+                return ("Nachmittag");
+            } else if (a >= 16 && a < 20) {
+                return ("Abend");
+            }
             return ("Nacht");
         }
         return ("Keine Gültige Uhrzeit");
