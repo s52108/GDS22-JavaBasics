a08fdba1b4cac69bb89da741600bfd58b69a15d6
postl <s52108@edu.campus02.at>
2022-09-24 12:03:42 +0200

    Pre - & Post Increment

--- ArithmetischeOperationen.java
+++ ArithmetischeOperationen.java
@@ -34,7 +34,18 @@ public class ArithmetischeOperationen {
         //Restwert/Modolo
         Ergebnis = a % b;
         System.out.println("Zahl a % Zahl b = " + Ergebnis);
-//
 
+        //a++ -> Bedeutet a = a + 1
+        System.out.println("a++");
+        //ich erwarte mir a
+        System.out.println(a++);
+        System.out.println("a = " + a);
+
+        //++b
+        System.out.println("++b");
+        System.out.println(++b);
+        System.out.println("b = " + b);
+
+        //STRG + K = Commit Shortcut
     }
 }
