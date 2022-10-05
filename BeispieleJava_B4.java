f2fdeb96b508a1aaf7360dc539603036f2f3e47b
postl <s52108@edu.campus02.at>
2022-10-05 12:13:00 +0200

    Added BeispieleJava_B4

---
+++ BeispieleJava_B4.java
@@ -0,0 +1,15 @@
+public class BeispieleJava_B4 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 100;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Durch 3 telbar: " + i);
+            }
+
+
+        }
+
+    }
+}
